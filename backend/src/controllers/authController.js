69e873a23db48c90ba2e4ddb919c2d953fbe0d21
NEELSAMELL369 <neelsamel369@gmail.com>
2025-11-09 02:30:33 +0530

    minor updated cookies and database

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -81,7 +81,7 @@ export const loginUser = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false, // true in production
+      secure: true,
       sameSite: "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
