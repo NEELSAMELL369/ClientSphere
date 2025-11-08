db8274c7f75ecdea7b407becef76c3984e984500
NEELSAMELL369 <neelsamel369@gmail.com>
2025-11-09 02:41:51 +0530

    prisma Schmea updated to linux env

--- backend/src/config/prismaClient.js
+++ backend/src/config/prismaClient.js
@@ -4,8 +4,11 @@ import { PrismaClient } from "@prisma/client";
 let prisma;
 
 if (!global.prisma) {
-  global.prisma = new PrismaClient();
+  global.prisma = new PrismaClient({
+    log: process.env.NODE_ENV === "development" ? ["query", "info", "warn", "error"] : []
+  });
 }
 
 prisma = global.prisma;
+
 export default prisma;
