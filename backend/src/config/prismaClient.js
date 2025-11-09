8a77df8480e02b1f25d748dc6a8656855d273b14
NEELSAMELL369 <neelsamel369@gmail.com>
2025-11-09 14:34:11 +0530

    changes made in env ssl certificate

--- backend/src/config/prismaClient.js
+++ backend/src/config/prismaClient.js
@@ -1,14 +1,17 @@
-// config/prismaClient.js
 import { PrismaClient } from "@prisma/client";
 
 let prisma;
 
 if (!global.prisma) {
   global.prisma = new PrismaClient({
-    log: process.env.NODE_ENV === "development" ? ["query", "info", "warn", "error"] : []
+    log: process.env.NODE_ENV === "development" ? ["query", "info", "warn", "error"] : [],
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL
+      }
+    }
   });
 }
 
 prisma = global.prisma;
-
 export default prisma;
